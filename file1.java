5df839f737054be3839eadf90e48bc945b9099f6
sanjiv <sanjiv@devopsguru.in>
2022-01-29 16:45:30 +0000

    my first commit of the source code

---
+++ file1.java
@@ -0,0 +1,9 @@
+total 3248
+-rw-rw-r-- 1 sanjivmonuredif sanjivmonuredif       6 Jan 29 16:35 check_idle
+drwxr-xr-x 2 jenkins         jenkins            4096 Jan 29 15:29 hsperfdata_jenkins
+drwxr-xr-x 2 root            root               4096 Jan 29 15:29 hsperfdata_root
+drwx------ 2 jenkins         jenkins            4096 Jan 29 15:29 jetty-0_0_0_0-8080-war-_-any-2699796513093264875
+drwx------ 2 sanjivmonuredif sanjivmonuredif    4096 Jan 29 15:30 ssh-LjyZvxWkmrsV
+drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-d1f53ccb2a43466f9fb8e6877c1c8810-rtkit-daemon.service-NhOxdn
+drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-d1f53ccb2a43466f9fb8e6877c1c8810-systemd-timesyncd.service-77w5U1
+-rw-r--r-- 1 jenkins         jenkins         3293397 Jan 29 15:29 winstone2234280389939604558.jar
---
+++ file2.java
@@ -0,0 +1,8 @@
+check_idle
+hsperfdata_jenkins
+hsperfdata_root
+jetty-0_0_0_0-8080-war-_-any-2699796513093264875
+ssh-LjyZvxWkmrsV
+systemd-private-d1f53ccb2a43466f9fb8e6877c1c8810-rtkit-daemon.service-NhOxdn
+systemd-private-d1f53ccb2a43466f9fb8e6877c1c8810-systemd-timesyncd.service-77w5U1
+winstone2234280389939604558.jar
