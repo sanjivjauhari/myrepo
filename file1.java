e99e1f7e31a4001af06bbf55f25692cbd8bb2b7c
sanjiv <sanjiv@devopsguru.in>
2022-01-29 16:59:11 +0000

    my third commit

--- file1.java
+++ file1.java
@@ -7,3 +7,4 @@ drwx------ 2 sanjivmonuredif sanjivmonuredif    4096 Jan 29 15:30 ssh-LjyZvxWkmr
 drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-d1f53ccb2a43466f9fb8e6877c1c8810-rtkit-daemon.service-NhOxdn
 drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-d1f53ccb2a43466f9fb8e6877c1c8810-systemd-timesyncd.service-77w5U1
 -rw-r--r-- 1 jenkins         jenkins         3293397 Jan 29 15:29 winstone2234280389939604558.jar
+getting ready for the third commit
